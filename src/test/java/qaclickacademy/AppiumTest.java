44911fa85266fcb0df3105cd53e05b5a8f6ee19b
kaja <kaja.mydn21@gmail.com>
2018-11-30 00:06:52 +0530

    firstc commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndroi()
+	{
+		System.out.println("NativeAppAndroi");
+	}
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+}
